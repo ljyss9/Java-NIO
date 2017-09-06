ffb76bdc34653026b61732306ddec55ef3e21939
ljyss92 <ljyss92@163.com>
2017-09-06 17:56:18 +0800

    commit for test NIO

---
+++ src/Handler.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+import java.nio.channels.SelectionKey;
+
+/**
+ * Created by ljy on 2017/9/6.
+ */
+public interface Handler {
+
+    void handleAccept(SelectionKey key) throws IOException;
+    void handleRead(SelectionKey key) throws IOException;
+    void handleWriter(SelectionKey key) throws  IOException;
+
+}
